a32afced75ccf78016f92ccb6075d146cd6558d4
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-02-07 15:03:31 -0300

    Added some comments on the methods

--- game/hider.py
+++ game/hider.py
@@ -23,9 +23,9 @@ class Hider:
         self._words =['cheese', 'summer', 'holiday', 'computer', 'mother', 'father', 'carpet']
         self._secret_word=''
         self._guesses=''
-        self.chosen_letters=['']
+        self.chosen_letters=['']    # This should change for self._chosen_letters = [] otherwise [''] is not an empty list
         
-    def chose_secret_word(self):
+    def choose_secret_word(self):
         """Choose the secret word from the list of possible words.
 
         Args:
@@ -35,6 +35,8 @@ class Hider:
             str: String representation of the word chosen.
         """
         self._secret_word= random.choice(self._words)
+        # Initialize _guesses with as many _ as the _secret_word's length
+        self._guesses = '_' * len(self._secret_word)
         return self._secret_word
         
     
@@ -48,14 +50,21 @@ class Hider:
         Return:
             str: String representation of guesses.
         """
-        
+        # Instead of this
         list_of_letters=list(self._secret_word)
         for i in range(len(list_of_letters)):
             if list_of_letters[i]==letter:
-                founded=str(i)
-                self._guesses+=founded
+                founded=str(i)  # i is an int
+                self._guesses+=founded  # this will append a number, not a letter
             else:
                 self._guesses+='_ '
+                
+        # We can do this
+        for i, c  in enumerate(self._secret_word):
+            # i is the index, c is the letter
+            if letter == c:
+                self._guesses[i] = c
+            
         return self._guesses
 
     def add_chosen_letter(self, letter):
